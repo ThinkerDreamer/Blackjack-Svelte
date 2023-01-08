fbe960d6145c238bd7bf1153fe8c726df0011c28
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-08 18:10:52 +0100

    add randomCard generating funct

--- src/GameBoard.svelte
+++ src/GameBoard.svelte
@@ -1,7 +1,32 @@
 <script>
-    let cards = [1, 2, 3];
-    $: sum = cards.reduce((a, b) => (a + b));
+    import { currentBet } from "./store";
+
+    $: playerCards = [];
+    $: playerSum = playerCards.reduce((a, b) => a + b, 0);
+
+    function randomCard() {
+        let card;
+        let randomNumber = Math.floor(Math.random() * 13) + 1;
+        if (randomNumber > 10) {
+            card = 10;
+        } else if (randomNumber === 1) {
+            card = 11;
+        } else {
+            card = randomNumber;
+        }
+        playerCards.push(card);
+        playerCards = playerCards;
+    }
+    console.log($currentBet);
 </script>
 
-<p>{cards}</p>
-<p>{sum}</p>
+<p class="small">Current bet: ${$currentBet}</p>
+<p>Cards: {playerCards}</p>
+<p>Sum: {playerSum}</p>
+<button on:click={randomCard}>Get a card</button>
+
+<style>
+    .small {
+        font-size: 50%;
+    }
+</style>
