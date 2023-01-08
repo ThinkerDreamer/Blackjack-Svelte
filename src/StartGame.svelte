7a5e2c9961aabb748b1fc50b769e92b80bb065ce
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-08 18:11:38 +0100

    create StartGame component for start button

---
+++ src/StartGame.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { bettingTime, gameRunning } from "./store";
+
+    function setBet() {
+        console.log("Setting bettingTime to true");
+        bettingTime.set(true);
+        document.getElementById("newGameBtn").style.display = "none";
+    }
+</script>
+
+<button on:click={setBet} id="newGameBtn">Start a new game</button>
