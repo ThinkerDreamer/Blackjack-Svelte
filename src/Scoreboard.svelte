b32ce671f368cb884f2a513adf16574cea97f889
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 10:31:49 +0100

    add scoreboard component

---
+++ src/Scoreboard.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { playerMoney } from "./store";
+</script>
+
+<p>You have ${$playerMoney}</p>
+
+<style>
+    p {
+        font-weight: bold;
+        font-size: 0.6rem;
+    }
+</style>
