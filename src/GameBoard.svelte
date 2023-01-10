202765897e0cc7245d574fa83c79c6c70afcbca9
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 19:21:18 +0100

    fix unneeded $'s, import playerCards

--- src/GameBoard.svelte
+++ src/GameBoard.svelte
@@ -1,29 +1,30 @@
 <script>
-  import { currentBet } from "./store";
+  import { currentBet, playerCards } from "./store";
   import HitButton from "./HitButton.svelte";
   import StayButton from "./StayButton.svelte";
   import FoldButton from "./FoldButton.svelte";
 
-  $: playerCards = [];
-  $: playerSum = playerCards.reduce((a, b) => a + b, 0);
+  $: playerSum = $playerCards.reduce((a, b) => a + b, 0);
   $: playersTurn = true;
-  $: canHit = false;
-  $: canStay = false;
-  $: canFold = false;
+  $: canHit = true;
+  $: canStay = true;
+  $: canFold = true;
 </script>
 
 <p class="small">Current bet: ${$currentBet}</p>
-<p>Cards: {playerCards}</p>
+<p>Cards: {$playerCards}</p>
 <p>Sum: {playerSum}</p>
 
-{#if $playersTurn}
-  {#if $canHit}
+{@debug playersTurn}
+
+{#if playersTurn}
+  {#if canHit}
     <HitButton />
   {/if}
-  {#if $canStay}
+  {#if canStay}
     <StayButton />
   {/if}
-  {#if $canFold}
+  {#if canFold}
     <FoldButton />
   {/if}
 {/if}
