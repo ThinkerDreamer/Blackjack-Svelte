00abf85a599df631447a9657262d1a26f9962ac0
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 17:52:16 +0100

    add dealerScore

--- src/store.js
+++ src/store.js
@@ -4,3 +4,4 @@ export let gameRunning = writable(false);
 export let bettingTime = writable(false);
 export let playerMoney = writable(200);
 export let currentBet = writable(25);
+export let dealerScore = writable(0);
