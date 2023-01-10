9880809c0184b5ab709ded9f500bf15497733c39
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 19:19:49 +0100

    use a store for the player's hand

--- src/HitButton.svelte
+++ src/HitButton.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { playerCards } from "./store";
+
   function randomCard() {
     let card;
     let randomNumber = Math.floor(Math.random() * 13) + 1;
@@ -9,8 +11,10 @@
     } else {
       card = randomNumber;
     }
-    playerCards.push(card);
-    playerCards = playerCards;
+    // add the new card to the rest of the cards in the player's hand
+    playerCards.update((currentCards) => {
+      return [...currentCards, card];
+    });
   }
 </script>
 
