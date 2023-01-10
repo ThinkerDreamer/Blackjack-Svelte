1862d4fda59de7018bb644cb224530e8b9a0372b
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 16:04:54 +0100

    refactor out HitButton into component

--- src/GameBoard.svelte
+++ src/GameBoard.svelte
@@ -1,32 +1,26 @@
 <script>
-    import { currentBet } from "./store";
+  import { currentBet } from "./store";
+  import HitButton from "./HitButton.svelte";
 
-    $: playerCards = [];
-    $: playerSum = playerCards.reduce((a, b) => a + b, 0);
+  $: playerCards = [];
+  $: playerSum = playerCards.reduce((a, b) => a + b, 0);
+  $: playersTurn = true;
 
-    function randomCard() {
-        let card;
-        let randomNumber = Math.floor(Math.random() * 13) + 1;
-        if (randomNumber > 10) {
-            card = 10;
-        } else if (randomNumber === 1) {
-            card = 11;
-        } else {
-            card = randomNumber;
-        }
-        playerCards.push(card);
-        playerCards = playerCards;
-    }
-    console.log($currentBet);
+  function checkForWinner() {
+    playersTurn = playersTurn;
+  }
 </script>
 
 <p class="small">Current bet: ${$currentBet}</p>
 <p>Cards: {playerCards}</p>
 <p>Sum: {playerSum}</p>
-<button on:click={randomCard}>Get a card</button>
+
+{#if $playersTurn}
+  <HitButton />
+{/if}
 
 <style>
-    .small {
-        font-size: 50%;
-    }
+  .small {
+    font-size: 50%;
+  }
 </style>
---
+++ src/HitButton.svelte
@@ -0,0 +1,17 @@
+<script>
+  function randomCard() {
+    let card;
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+      card = 10;
+    } else if (randomNumber === 1) {
+      card = 11;
+    } else {
+      card = randomNumber;
+    }
+    playerCards.push(card);
+    playerCards = playerCards;
+  }
+</script>
+
+<button on:click={randomCard}>Get a card</button>
