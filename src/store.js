ea96e4d0f6d30808837b262677b51a6eba4fe0db
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 19:20:16 +0100

    add playerCards to stores

--- src/store.js
+++ src/store.js
@@ -5,3 +5,4 @@ export let bettingTime = writable(false);
 export let playerMoney = writable(200);
 export let currentBet = writable(25);
 export let dealerScore = writable(0);
+export let playerCards = writable([]);
