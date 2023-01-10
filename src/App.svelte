eb40e7ad0c19bcfd941f5ad30a5bcc83ccce5c5b
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-10 16:07:51 +0100

    [skip render] linting minor edit

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,56 @@
 <script>
-	import { gameRunning, bettingTime, playerMoney } from "./store.js";
+  import { gameRunning, bettingTime, playerMoney } from "./store.js";
 
-	import Title from "./Title.svelte";
-	import Message from "./Message.svelte";
-	import Betting from "./Betting.svelte";
-	import GameBoard from "./GameBoard.svelte";
-	import StartGame from "./StartGame.svelte";
-	import Scoreboard from "./Scoreboard.svelte";
+  import Title from "./Title.svelte";
+  import Message from "./Message.svelte";
+  import Betting from "./Betting.svelte";
+  import GameBoard from "./GameBoard.svelte";
+  import StartGame from "./StartGame.svelte";
+  import Scoreboard from "./Scoreboard.svelte";
 
-	$: showMessage = true;
+  $: showMessage = true;
 </script>
 
 <svelte:head>
-	<title>Blackjack in Svelte</title>
+  <title>Blackjack in Svelte</title>
 </svelte:head>
 
 <Title />
 
 {#if showMessage && !$gameRunning && !$bettingTime}
-	<Message />
+  <Message />
 {/if}
 
 {#if !$gameRunning && !$bettingTime}
-	<StartGame />
+  <StartGame />
 {/if}
 
 {#if $bettingTime}
-	<Betting />
+  <Betting />
 {/if}
 
 {#if $gameRunning}
-	<GameBoard />
-	<Scoreboard />
+  <GameBoard />
+  <Scoreboard />
 {/if}
 
 <style>
-	:root {
-		font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
-			Arial, sans-serif;
-		background-image: url("../blackjack-table.jpg");
-		/*             filter: blur(8px);
+  :root {
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", Arial,
+      sans-serif;
+    background-image: url("../blackjack-table.jpg");
+    /*             filter: blur(8px);
             -webkit-filter: blur(8px); */
-		background-size: cover;
-		font-weight: bold;
-		color: white;
-		text-align: center;
-		max-width: 700px;
-		margin: 0 auto;
-		font-size: 40px;
-	}
-	:global(p) {
-		margin: 0.5rem auto;
-		color: white;
-	}
+    background-size: cover;
+    font-weight: bold;
+    color: white;
+    text-align: center;
+    max-width: 700px;
+    margin: 0 auto;
+    font-size: 40px;
+  }
+  :global(p) {
+    margin: 0.5rem auto;
+    color: white;
+  }
 </style>
