3900f4ebbd9acced68379701152ad87407d32dc3
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-11 19:44:29 +0100

    add dealer play logic

--- src/StayButton.svelte
+++ src/StayButton.svelte
@@ -1,9 +1,46 @@
 <script>
-    import { dealerScore } from "./store";
+    import { playerSum, dealerSum, currentMsg, playerMoney } from "./store.js";
+    import {
+        giveCardDealer,
+        handlePlayerWins,
+        handleDealerWins,
+        handleDraw,
+    } from "./utils";
 
     function calculateDealerScore() {
-        dealerScore.set(17); // placeholder value for testing
-        alert("You are staying and the dealer got " + $dealerScore);
+        dealerPlay();
+        if ($dealerSum <= 21 && $playerSum < $dealerSum) {
+            currentMsg.set(
+                "Dealer got " + $dealerSum + ". You lose! Play again?"
+            );
+            handleDealerWins();
+        } else if ($dealerSum > 21) {
+            currentMsg.set(
+                "Dealer busted with " + $dealerSum + "! You win! Play again?"
+            );
+            console.log(`playerMoney is: ${$playerMoney}`);
+            handlePlayerWins();
+        } else if ($playerSum > $dealerSum) {
+            currentMsg.set(
+                "You win! Dealer got " + $dealerSum + ". Play again?"
+            );
+            handlePlayerWins();
+        } else if ($playerSum === $dealerSum) {
+            currentMsg.set(
+                "It's a draw! Dealer got " + $dealerSum + ". Play again?"
+            );
+            handleDraw();
+        }
+    }
+
+    function dealerPlay() {
+        while ($dealerSum < 21) {
+            if ($dealerSum >= 17) {
+                return $dealerSum;
+            } else {
+                giveCardDealer();
+            }
+        }
     }
 </script>
 
