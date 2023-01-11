de4e519bae5cc477e663064e3c7e9e7295feaebf
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-11 19:39:42 +0100

    create derived sum stores

--- src/store.js
+++ src/store.js
@@ -1,8 +1,44 @@
-import { writable } from 'svelte/store'
+import { writable, derived } from 'svelte/store'
 
 export let gameRunning = writable(false);
 export let bettingTime = writable(false);
 export let playerMoney = writable(200);
 export let currentBet = writable(25);
-export let dealerScore = writable(0);
 export let playerCards = writable([]);
+export let dealerCards = writable([]);
+export let currentMsg = writable("");
+export let playersTurn = writable(false);
+
+export const playerSum = derived(
+    playerCards,
+    $playerCards => {
+        let aceFlag = false;
+        for (let card of $playerCards) {
+            if (card.name === "A") {
+                aceFlag = true;
+            }
+        }
+        let initialSum = $playerCards.reduce((acc, obj) => acc + obj.value, 0)
+        if (initialSum > 21 && aceFlag) {
+            initialSum -= 10;
+        }
+        return initialSum;
+    }
+);
+
+export const dealerSum = derived(
+    dealerCards,
+    $dealerCards => {
+        let aceFlag = false;
+        for (let card of $dealerCards) {
+            if (card.name === "A") {
+                aceFlag = true;
+            }
+        }
+        let initialSum = $dealerCards.reduce((acc, obj) => acc + obj.value, 0)
+        if (initialSum > 21 && aceFlag) {
+            initialSum -= 10;
+        }
+        return initialSum;
+    }
+);
