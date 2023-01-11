c6cf25f207ee4d75f0b5b45d18b8f9e937f75115
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-11 19:12:20 +0100

    remove - there's no fold in blackjack :facepalm:

--- src/FoldButton.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { dealerScore } from "./store";
-
-    function handleFold() {
-        alert("You folded.") // placeholder value for testing
-    }
-</script>
-
-<button on:click={handleFold}>Fold</button>
