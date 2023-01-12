9e50c649b187bd21a847ee3fcff1605ad32579b0
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-12 18:07:10 +0100

    add commas between cards, add font-size prop

--- src/CardView.svelte
+++ src/CardView.svelte
@@ -4,8 +4,18 @@
 
 <p>
     Cards:
-    {#each $playerCards as card}
-        <span>{card.name} </span>
+    {#each $playerCards as card, index}
+        {@html index == $playerCards.length - 1
+            ? `<span>${card.name}</span>`
+            : `<span>${card.name}, </span>`}
     {/each}
 </p>
-<p>Sum: {$playerSum}</p>
+<p>
+    Sum: {$playerSum}
+</p>
+
+<style>
+    p {
+        font-size: var(--prop-size, 1rem);
+    }
+</style>
