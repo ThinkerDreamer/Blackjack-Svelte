68a0809f6dc87537c5e9d8c7dbf87ccc38874f54
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-01-13 13:38:07 +0100

    don't deal player cards before setting bet

--- src/StartGame.svelte
+++ src/StartGame.svelte
@@ -5,15 +5,12 @@
         dealerCards,
         showCardView,
     } from "./store.js";
-    import { giveCardPlayer } from "./utils";
 
     function handleStartBtn() {
         showCardView.set(false);
         gameRunning.set(true);
         playerCards.set([]);
         dealerCards.set([]);
-        giveCardPlayer();
-        giveCardPlayer();
     }
 </script>
 
